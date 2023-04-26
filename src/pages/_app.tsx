5b876172f572181d1d11fb43a71f6e826774c23e
attila-tamas <tamasattila02@gmail.com>
2023-04-26 14:24:59 +0200

    Change the default font to "Quicksand"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Quicksand } from "next/font/google";
+
+const quicksand = Quicksand({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="icon" type="image/png" href="/favicon.png" />
+			</Head>
+
+			{/* Set the default font for the application */}
+			<style jsx global>{`
+				html {
+					font-family: ${quicksand.style.fontFamily};
+				}
+			`}</style>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-				<link rel="icon" type="image/png" href="/favicon.png" />
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
