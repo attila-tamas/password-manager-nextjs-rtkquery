8f91303914ecdf4c0d36f2ded6e25f312d6b60ac
attila-tamas <tamasattila02@gmail.com>
2023-04-26 13:53:57 +0200

    Change the favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="icon" type="image/png" href="/favicon.png" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
