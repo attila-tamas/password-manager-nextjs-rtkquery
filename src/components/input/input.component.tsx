f065de64fb91640761d4ca7d8a916049e3e59b10
attila-tamas <tamasattila02@gmail.com>
2023-06-27 10:20:52 +0200

    Add "validation" support to "input" component

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -1,6 +1,13 @@
 import styles from "./input.module.scss";
 // react
 import { MutableRefObject, ReactNode } from "react";
+// @hooks
+import { Validation } from "@hooks/useValidation";
+// @components
+import Icon, { icons } from "@components/icon/icon";
+import Spinner from "@components/spinner/spinner.component";
+// @util
+import { pixelToEm } from "@util/pixelConverter";
 
 type Props = {
 	type: string;
@@ -13,10 +20,10 @@ type Props = {
 	reference?: MutableRefObject<HTMLInputElement | null>;
 	showPassword?: boolean;
 	copyButton?: boolean;
-	error?: boolean;
 	className?: string;
 	children?: ReactNode;
 	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	validation?: Validation;
 };
 
 export default function Input({
@@ -29,24 +36,55 @@ export default function Input({
 	name,
 	reference,
 	showPassword,
-	error,
 	className,
 	children,
 	onChange,
+	validation,
 }: Props) {
+	function inputClasses(): string {
+		let classes = `${styles["input"]} ${className}`;
+
+		if (validation?.errorMsg) classes += ` ${styles["input--error"]}`;
+
+		return classes;
+	}
+
 	function inputType(): string {
 		if (type === "password" && showPassword) return "text";
 		return type;
 	}
 
+	function validationIcon(): undefined | JSX.Element {
+		if (validation?.isLoading) {
+			return <Spinner size={24} />;
+		} else if (validation?.isSuccess) {
+			return (
+				<Icon
+					icon={icons.tick}
+					size={pixelToEm(24)}
+					color="var(--color-primary)"
+				/>
+			);
+		} else if (validation?.errorMsg) {
+			return (
+				<Icon
+					icon={icons.cross}
+					size={pixelToEm(24)}
+					color="var(--color-error)"
+				/>
+			);
+		}
+		return;
+	}
+
+	// make sure aria-invalid is either true or false
+	function isAriaInvalid(): boolean {
+		if (validation?.errorMsg) return true;
+		return false;
+	}
+
 	return (
-		<div
-			className={`
-				${styles["input"]}
-				${error && styles["input--error"]}
-				${className}
-			`}
-		>
+		<div className={inputClasses()}>
 			<input
 				type={inputType()}
 				value={value}
@@ -57,9 +95,10 @@ export default function Input({
 				ref={reference}
 				maxLength={maxLength}
 				onChange={onChange}
-				aria-invalid={error}
+				aria-invalid={isAriaInvalid()}
 			/>
 			{children}
+			{validationIcon()}
 		</div>
 	);
 }
