d85af54bc88067e122e91ad2fa963dfd9ac8800b
attila-tamas <tamasattila02@gmail.com>
2023-06-28 19:41:16 +0200

    Improve "snackbar" components

--- src/components/snackbars/snackbarContext.tsx
+++ src/components/snackbars/snackbarContext.tsx
@@ -1,29 +1,25 @@
 // npm
 import { SnackbarProvider } from "notistack";
 // @components
-import ErrorSnackbar from "./snackbarError/snackbarError.component";
-import SuccessSnackbar from "./snackbarSuccess/snackbarSuccess.component";
+import ErrorSnackbar from "./snackbarError.component";
+import SuccessSnackbar from "./snackbarSuccess.component";
 
-// snackbar context provider
 export default function SnackbarContext({ children }: any) {
 	return (
 		<SnackbarProvider
-			autoHideDuration={3000} // hide after 3 seconds
-			dense // denser margin between snackbars
-			preventDuplicate // prevent the same snackbar from showing multiple times at once
-			maxSnack={3} // maximum number of snackbars showing at once
+			autoHideDuration={3000} // 3 seconds
+			dense
+			preventDuplicate
+			maxSnack={3}
 			anchorOrigin={{
-				// snackbar origin position
 				vertical: "top",
 				horizontal: "center",
 			}}
 			Components={{
-				// snackbar variants
 				success: SuccessSnackbar,
 				error: ErrorSnackbar,
 			}}
 		>
-			{/* render the child components */}
 			{children}
 		</SnackbarProvider>
 	);
