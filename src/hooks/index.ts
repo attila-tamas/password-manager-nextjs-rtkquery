024ba9c7e9ab4ad8f69c7e3e31eb6736f0bdd54e
attila-tamas <tamasattila02@gmail.com>
2023-07-08 22:28:30 +0200

    Add "useFindCustomField" and "useLazyQuery" hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,14 @@
 import useClickOutside from "./useClickOutside";
+import useCopyToClipboard from "./useCopyToClipboard";
 import useDebounce from "./useDebounce";
 import useDispatchLogin from "./useDispatchLogin";
 import useDispatchLogout from "./useDispatchLogout";
 import useEffectOnMount from "./useEffectOnMount";
 import useEffectOnUpdate from "./useEffectOnUpdate";
 import useEventListener from "./useEventListener";
+import useFindCustomField from "./useFindCustomField";
 import useFormInput from "./useFormInput";
+import useLazyQuery from "./useLazyQuery";
 import useLiveValidation from "./useLiveValidation";
 import useMutation from "./useMutation";
 import useRenderCount from "./useRenderCount";
@@ -17,12 +20,14 @@ import useValidation from "./useValidation";
 
 export {
 	useClickOutside,
+	useCopyToClipboard,
 	useDebounce,
 	useDispatchLogin,
 	useDispatchLogout,
 	useEffectOnMount,
 	useEffectOnUpdate,
 	useEventListener,
+	useFindCustomField,
 	useFormInput,
 	useLiveValidation,
 	useMutation,
@@ -31,5 +36,6 @@ export {
 	useSuccess,
 	useTimeout,
 	useToggle,
+	useLazyQuery,
 	useValidation,
 };
