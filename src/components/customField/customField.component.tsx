1ce1b445fe8aac0f7c40a08f4b3aae30cbbcd313
attila-tamas <tamasattila02@gmail.com>
2023-07-08 22:25:12 +0200

    Add "customField" component

---
+++ src/components/customField/customField.component.tsx
@@ -0,0 +1,81 @@
+import useClickOutside from "@hooks/useClickOutside";
+import useCopyToClipboard from "@hooks/useCopyToClipboard";
+import useToggle from "@hooks/useToggle";
+import { pixelToEm } from "@util/pixelConverter";
+import { useRef } from "react";
+import { Icon, icons, Input } from "..";
+import styles from "./customField.module.scss";
+
+export type CustomFieldType = {
+	key: string;
+	value: string;
+};
+
+export default function CustomField({
+	field,
+	index,
+	onInputChange,
+	onRemoveCustomFieldClicked,
+}: {
+	field: CustomFieldType;
+	index: number;
+	onInputChange: (index: number, event: any) => void;
+	onRemoveCustomFieldClicked: (index: number) => void;
+}) {
+	const [copyToClipBoard, { success: copySuccess }] = useCopyToClipboard();
+
+	const isLabelEditable = useToggle(false);
+	const ref = useRef<HTMLDivElement>(null);
+
+	useClickOutside(ref, () => {
+		if (isLabelEditable.value) isLabelEditable.toggleValue();
+	});
+
+	return (
+		<div className={styles["custom-field"]}>
+			<div className={styles["custom-field__title"]} ref={ref}>
+				<Icon
+					icon={icons.edit}
+					size={pixelToEm(24)}
+					className="interactable"
+					onClick={() => isLabelEditable.toggleValue()}
+				/>
+
+				{isLabelEditable.value ? (
+					<Input
+						type="text"
+						name="key"
+						value={field.key}
+						onChange={(event: any) => onInputChange(index, event)}
+					/>
+				) : (
+					<label htmlFor={index.toString()}>{field.key}</label>
+				)}
+			</div>
+
+			<Input
+				type="text"
+				name="value"
+				value={field.value}
+				id={index.toString()}
+				onChange={(event: any) => onInputChange(index, event)}
+				className={styles["custom-field__input"]}
+			>
+				<Icon
+					icon={copySuccess ? icons.tick : icons.copy}
+					size={pixelToEm(24)}
+					className="interactable"
+					onClick={() => copyToClipBoard(field.value)}
+				/>
+
+				<Icon
+					icon={icons.trash}
+					size={pixelToEm(24)}
+					color="var(--color-error)"
+					className="interactable"
+					onClick={() => onRemoveCustomFieldClicked(index)}
+				/>
+			</Input>
+		</div>
+	);
+}
